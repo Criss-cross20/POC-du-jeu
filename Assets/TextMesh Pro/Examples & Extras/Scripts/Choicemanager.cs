ab8e2f0c490b7fb923d102eb7abbea7961c0726b
Criss-cross20 <2032263@champlaincollege.qc.ca>
2026-04-13 16:57:30 -0400

    Faire apparaitre le texte

--- Assets/TextMesh Pro/Examples & Extras/Scripts/Choicemanager.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/Choicemanager.cs
@@ -7,22 +7,40 @@ public class ChoiceManager : MonoBehaviour
     public GameObject option2Button;
     public GameObject backgroundButton;
 
-    public void ChooseOption1()
+    void Start()
     {
-        // Your game logic here
-        HideButtons();
+        //Hide them at the start
+        option1Button.SetActive(false);
+        option2Button.SetActive(false);
+        backgroundButton.SetActive(false);
+
+        //Show them when the game starts
+        ShowChoices();
     }
 
-    public void ChooseOption2()
+      public void ShowChoices()
     {
-        // Your game logic here
-        HideButtons();
+        option1Button.SetActive(true);
+        option2Button.SetActive(true);
+        backgroundButton.SetActive(true);
     }
 
-    void HideButtons()
+    public void HideChoices()
     {
         option1Button.SetActive(false);
         option2Button.SetActive(false);
         backgroundButton.SetActive(false);
+    }
+
+    public void ChooseOption1()
+    {
+        // whatever Option 1 does
+        HideChoices();
+    }
+
+    public void ChooseOption2()
+    {
+        // whatever Option 2 does
+        HideChoices();
     }
 }
