74d002a7d1c744e8fcc730fa171328b9d6ab1fd5
Criss-cross20 <2032263@champlaincollege.qc.ca>
2026-04-13 14:14:46 -0400

    2 options

---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/Choicemanager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+
+namespace TMPro.Examples
+{
+
+public class ChoiceManager : MonoBehaviour
+{
+    public void ChooseOption1()
+    {
+        Debug.Log("Player selected option 1");
+        // Add your logic here (e.g., apply damage, change scene, update dialogue)
+    }
+
+    public void ChooseOption2()
+    {
+        Debug.Log("Player selected option 2");
+        // Add your logic here
+    }
+}
+
+   }
+
