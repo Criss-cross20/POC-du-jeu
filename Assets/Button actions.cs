322ffda998a3698099df3a7e40ddbceeabdfdc1d
Criss-cross20 <2032263@champlaincollege.qc.ca>
2026-04-23 16:15:20 -0400

    script 2.0

--- Assets/Button actions.cs
+++ Assets/Button actions.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
+    public GameObject chose;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
